f089d593eee9aeea4060add2abfea057ab0fb56f
Brad  Trott <bradtrott@Brads-MacBook-Pro.local>
2017-10-24 12:03:50 -0500

    mobgit add .

--- FizzBuzz/Brain.swift
+++ FizzBuzz/Brain.swift
@@ -2,7 +2,7 @@
 //  Brain.swift
 //  FizzBuzz
 //
-//  Created by Brad  Trott on 10/23/17.
+//  Created by Brad  Trott on 10/24/17.
 //  Copyright © 2017 Brad  Trott. All rights reserved.
 //
 
@@ -11,7 +11,18 @@ import Foundation
 class Brain {
     
     func isDivisible(divisor: Int, number: Int) -> Bool {
-        return divisor % number == 0
+        return number % divisor == 0
     }
-
+    
+    func isDivisibleByThree(number: Int) -> Bool {
+        return isDivisible(divisor: 3, number: number)
+    }
+    
+    func isDivisibleByFive(number: Int) -> Bool {
+        return isDivisible(divisor: 5, number: number)
+    }
+    
+    func isDivisibleByFifteen(number: Int) -> Bool {
+        return isDivisible(divisor: 15, number: number)
+    }
 }
--- FizzBuzzTests/BrainTests.swift
+++ FizzBuzzTests/BrainTests.swift
@@ -7,39 +7,42 @@
 //
 
 import XCTest
-@testable import FizzBuzz
+
 
 class BrainTests: XCTestCase {
     
     let brain = Brain()
+
     
     func testIsDivisibleByThree() {
-        let result = brain.isDivisible(divisor: 3, number: 3)
+        let result = brain.isDivisibleByThree(number: 3)
         XCTAssertTrue(result)
+        
+        
     }
     
     func testIsNotDivisibleByThree() {
-        let result =  brain.isDivisible(divisor: 3, number: 4)
+        let result = brain.isDivisibleByThree(number: 4)
         XCTAssertFalse(result)
     }
     
     func testIsDivisibleByFive() {
-        let result = brain.isDivisible(divisor: 5, number: 5)
-        XCTAssertTrue(result);
+        let result = brain.isDivisibleByFive(number: 5)
+        XCTAssertTrue(result)
     }
-
+    
     func testIsNotDivisibleByFive() {
-        let result = brain.isDivisible(divisor: 5, number: 4)
-        XCTAssertFalse(result);
+               let result = brain.isDivisibleByFive(number: 8)
+        XCTAssertFalse(result)
     }
     
     func testIsDivisibleByFifteen() {
-        let result = brain.isDivisible(divisor: 15, number: 15)
-        XCTAssertTrue(result);
+        let result = brain.isDivisibleByFifteen(number: 15)
+        XCTAssertTrue(result)
     }
     
     func testIsNotDivisibleByFifteen() {
-        let result = brain.isDivisible(divisor: 15, number: 12)
-        XCTAssertFalse(result);
+        let result = brain.isDivisibleByFifteen(number:12)
+        XCTAssertFalse(result)
     }
 }
